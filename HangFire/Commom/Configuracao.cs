2a919ab11b38113f3a790aa2a9ecd8c856ffb818
Juliano Zanella <juliano.zanella.live@gmail.com>
2020-10-29 10:25:18 -0300

    Solução de erro do commit

--- HangFire/Commom/Configuracao.cs
+++ HangFire/Commom/Configuracao.cs
@@ -18,5 +18,6 @@ namespace HangFire.RN.Commom
         public static string HangFireConnectionString { get { return AppSetting.GetConnectionString("HangfireConnection"); } }
         public static string AppConnectionString { get { return AppSetting.GetConnectionString("AppConnection"); } }
         public static string CaminhoLog { get { return AppSetting.GetSection("CaminhoLog").Value; } }
+        public static int DapperCommandTimeout { get { return int.Parse(AppSetting.GetSection("DapperCommandTimeout").Value); } }
     }
 }
